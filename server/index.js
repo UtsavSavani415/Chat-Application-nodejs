599797dac1830e6692bc33cf57744ce7815dde95
Utsav <utsavsavani415@gmail.com>
2023-09-20 15:56:13 +0530

    setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
+const uri = process.env.ATLAS_URI;
 
 app.use(express.json());
 app.use(cors());
 
+// CRUD
+
+app.post("/", (req, res) => {});
+
 app.listen(port, (req, res) => {
   console.log("Server running on port", port);
 });
- 
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }) 
+  .then(() => {
+    console.log("Mongo db connection established");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed: ", error);
+  });
