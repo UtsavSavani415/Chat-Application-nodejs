6c15e7d9ee2d21509bd8ce677d01030f13bf9b8b
Utsav <utsavsavani415@gmail.com>
2023-09-23 22:53:19 +0530

    user info with get api setted

--- client/src/context/ChatContext.jsx
+++ client/src/context/ChatContext.jsx
@@ -8,6 +8,29 @@ export const ChatContextProvider = ({ children, user }) => {
   const [userChats, setUserChats] = useState(null);
   const [isUserChatsLoading, setIsUserChatsLoading] = useState(false);
   const [userChatsError, setUserChatsError] = useState(null);
+  const [potentialChats, setPotentialChats] = useState([]);
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await getRequest(`${baseurl}/users`);
+
+      if (response.error) {
+        return console.log("error fetching users", response);
+      }
+
+      const pChats = response.find((u) => {
+        let isChatCreated = false;
+        if (user._id === u._id) {
+          return false;
+        }
+
+        if (userChats) {
+        }
+      });
+    };
+
+    getUsers();
+  }, []);
 
   useEffect(() => {
     const getUserChats = async () => {
